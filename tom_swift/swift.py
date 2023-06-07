1d499ff877e3efbbf956d9417b6ce0a6747309ed
Lindy Lindstrom <llindstrom@lco.global>
2023-06-07 18:24:20 +0000

    add Sam's base form to start with, start to add instrument TABs

--- tom_swift/swift.py
+++ tom_swift/swift.py
@@ -37,16 +37,77 @@ class SwiftObservationForm(BaseObservationForm):
 #            'groups'
 #        )
 #
+
+#
+# This is Sam's UVOTObservationForm
+
+# Fields for user to fill in in Swift ToO form.
+# Some fields are filled automatically by default for rapid submission of EM-GW candidates
+
+    urgency = forms.IntegerField(required=False, label='ToO Urgency',initial=2)
+    obs_type = forms.CharField(required=False, label='Observation type (Spectroscopy; Light curve; Position, Timing',initial='Light Curve')
+    opt_mag = forms.FloatField(required=False, label='Optical Magnitude')
+    opt_filt = forms.CharField(required=False, label='What filter was this measured in',initial='u')
+    exposure = forms.FloatField(required=False, label='Exposure time requested [s]',initial=500)
+    exp_time_just = forms.CharField(required=False, label='Time Justification', initial="500s to determine if source has faded")
+    num_of_visits= forms.IntegerField(required=False, label='Number of visits [integer]',initial=1)
+    monitoring_freq = forms.CharField(required=False, label='Frequency of visits')
+    exp_time_per_visit = forms.FloatField(required=False, label='Exposure time per visit(s) [s]')
+    uvot_mode = forms.CharField(required=False, label='UVOT filter mode (can write instructions or specific mode)', initial='0x01AB')
+    science_just = forms.CharField(required=False, label='Science Justification', initial="500s to determine if source has faded")
+    immediate_objective = forms.CharField(required=False, label='Immediate Objective',initial="To determine if source has faded.")
+    source_type=forms.CharField(required=False, label='Source Type',initial='Optical/UV transient in GW error region')
+    uvot_just = forms.CharField(required=False, label='UVOT Mode Justification', initial="Needs to be revised, do we want default as u or all filters?")
+
+    def layout(self):
+        return Layout(
+            'source_type',
+            'urgency',
+            'obs_type',
+            'opt_mag',
+            'opt_filt',
+            'exposure',
+            'num_of_visits',
+            HTML('<p>If number of visits more than one change next exposure time per visit and monitoring frequency, otherwise leave blank.</p>'),
+            'exp_time_per_visit',
+            'monitoring_freq',
+            'exp_time_just',
+            'immediate_objective',
+            'science_just',
+        )
+
+class SwiftUVOTObservationForm(SwiftObservationForm):
+    """Extends the SwiftObservationForm with UVOT-specific fields
+    """
+    def layout(self):
+        layout = super().layout()
+        layout.append('uvot_mode')
+        layout.append('uvot_just')
+        return layout
+
+class SwiftXRTObservationForm(SwiftObservationForm):
     pass
 
-class SwiftFacility:
+class SwiftBATObservationForm(SwiftObservationForm):
+    pass
+
+
+class SwiftFacility(BaseObservationFacility):
+    def __init__(self):
+        super().__init__()
+        self.swift_api = SwiftAPI()
+
     name = 'Swift'
     observation_types = [
         ('OBSERVATION', 'Custom Observation')
     ]
 
+    # observation_forms key-values become TABs in the observation_form template
     observation_forms = {
-        'OBSERVATION': SwiftObservationForm
+        # TODO: this should be driven by swift_api.SWIFT_INSTRUMENT_CHOICES
+        'UVOT': SwiftUVOTObservationForm,
+        'XRT': SwiftXRTObservationForm,
+        'BAT': SwiftBATObservationForm,
     }
     template_name = 'tom_swift/observation_form.html'
 
